a00ebdbd398d748a3b8bbacf67133fcc523b48d2
Lucia <2313970630@qq.com>
2020-07-08 22:49:07 +0800

    .

---
+++ main.js
@@ -0,0 +1,167 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+autoSetCanvasSize(canvas)
+listenToUser(canvas)
+
+/****/
+black.onclick = function() {
+    ctx.fillStyle = 'black';
+    ctx.strokeStyle = 'black';
+    black.classList.add('active');
+    red.classList.remove('active');
+    green.classList.remove('active');
+    blue.classList.remove('active');
+}
+red.onclick = function() {
+    ctx.fillStyle = 'red';
+    ctx.strokeStyle = 'red';
+    red.classList.add('active');
+    green.classList.remove('active');
+    blue.classList.remove('active');
+    black.classList.remove('active');
+}
+green.onclick = function() {
+    ctx.fillStyle = 'green';
+    ctx.strokeStyle = 'green';
+    green.classList.add('active');
+    red.classList.remove('active');
+    blue.classList.remove('active');
+    black.classList.remove('active');
+}
+blue.onclick = function() {
+    ctx.fillStyle = 'blue';
+    ctx.strokeStyle = 'blue';
+    blue.classList.add('active');
+    green.classList.remove('active');
+    red.classList.remove('active');
+    black.classList.remove('active');
+}
+
+/****/
+function drawCircle(x, y, radius) {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, (Math.PI / 180) * 1, true)
+    ctx.fill()
+}
+
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.lineWidth = 10;
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke()
+
+}
+
+function autoSetCanvasSize(canvas) {
+    resizeWindow()
+
+    window.onresize = function() {
+        resizeWindow()
+    }
+
+    function resizeWindow() {
+        var pageWidth = document.documentElement.clientWidth
+        var pageHeight = document.documentElement.clientHeight
+        canvas.width = pageWidth
+        canvas.height = pageHeight
+    }
+}
+
+function listenToUser(canvas) {
+    var using = false;
+    var time;
+    var lastPoint = {
+        x: undefined,
+        y: undefined
+    }
+    var eraserEnabled = false;
+    brush.onclick = function() {
+        eraserEnabled = false;
+        brush.classList.add('active');
+        eraser.classList.remove('active');
+    }
+    eraser.onclick = function() {
+        eraserEnabled = true;
+        eraser.classList.add('active');
+        brush.classList.remove('active');
+    }
+
+    //特性检测，检测的不是设备，而是特性
+    if (document.body.ontouchstart !== undefined) {
+        //触屏设备
+        canvas.ontouchstart = function(e) {
+            using = true;
+            var x = e.touches[0].clientX;
+            var y = e.touches[0].PIclientY;
+
+            if (eraserEnabled) {
+                ctx.clearRect(x, y, 10, 10)
+            } else {
+                lastPoint = {
+                    "x": x,
+                    "y": y
+                };
+                drawCircle(x, y, 5);
+            }
+        }
+        canvas.ontouchmove = function(e) {
+            if (using) {
+                var x = e.touches[0].clientX;
+                var y = e.touches[0].clientY;
+                if (eraserEnabled) {
+                    ctx.clearRect(x, y, 10, 10)
+                } else {
+                    var newPoint = {
+                        "x": x,
+                        "y": y
+                    }
+                    drawCircle(x, y, 5);
+                    drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+                    lastPoint = newPoint;
+                }
+            }
+        }
+        canvas.ontouchend = function(e) {
+            using = false;
+        }
+    } else {
+        //L
+        document.onmousedown = function(e) {
+            using = true;
+            var x = e.clientX;
+            var y = e.clientY;
+
+            if (eraserEnabled) {
+                ctx.clearRect(x, y, 10, 10)
+            } else {
+                lastPoint = {
+                    "x": x,
+                    "y": y
+                };
+                drawCircle(x, y, 5)
+            }
+        }
+        document.onmousemove = function(e) {
+            if (using) {
+                var x = e.clientX;
+                var y = e.clientY;
+                if (eraserEnabled) {
+                    ctx.clearRect(x, y, 10, 10)
+                } else {
+                    var newPoint = {
+                        "x": x,
+                        "y": y
+                    }
+                    drawCircle(x, y, 5)
+                    drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+                    lastPoint = newPoint;
+                }
+            }
+        }
+        document.onmouseup = function(e) {
+            using = false;
+        }
+    }
+
+}
